5e20736906c669643bf2a49243bfc4191f97281e
maryzam <zamo.code@gmail.com>
2018-10-07 23:04:52 +0300

    Switch between All/Male/Female top names

--- draft/topNamesByGenre.js
+++ draft/topNamesByGenre.js
@@ -1,9 +1,9 @@
-d3.tsv("data/top_10_by_genre.tsv")
+d3.json("data/top_10_by_genre.json")
 	.then((source) => {
 
 		const width = 960;
   		const height = 400;
-  		const headOffset = 100;
+  		const headOffset = 70;
 
   		// prepare
 		const genres = getGenres(source);
@@ -27,71 +27,101 @@ d3.tsv("data/top_10_by_genre.tsv")
 							.attr("class", "header");
 
 		header
-			.append("text")
-			.text("Top 10 Names by Genre")
-			.attr("class", "title")
-			.attr("transform", `translate(${width / 2}, ${headOffset / 3})`);
-
-		header
 			.selectAll(".genre")
 				.data(genres).enter()
 			.append("g")
 				.attr("class", "genre")
-				.attr("transform", (d) => `translate(${scaleGenres(d)},${ headOffset * 2 / 3})`)
+				.attr("transform", (d) => `translate(${scaleGenres(d)},${ headOffset / 3})`)
 			.append("text")
 				.text((d) => d);
 
 		// content
-		const itemHeight = Math.floor((height - headOffset) / 10) - 5;
-		const itemWidth = Math.floor(scaleGenres.step()) - 5;
+		const itemHeight = Math.floor((height - headOffset) / 10);
+		const itemWidth = Math.floor(scaleGenres.step()) - 10;
 
 		const content = svg.append("g")
 						.attr("class", "content")
 						.attr("transform", `translate(0, ${headOffset - itemHeight})`);
 
-		const orders = {};
-		source.forEach((d) => {
-			const genre = d.Genre;
-			if (!orders[genre]) {
-				orders[genre] = {
-					All: 0,
-					Female: 0,
-					Male: 0
-				};
-			}
-			const info = orders[genre];
-			const sex = d.Sex == "F" ? "Female" : "Male";
-			d["Rating"] = {	
-				All: info.All,
-				Gender: info[sex]
-			}
-			info.All++;
-			info[sex]++;
-		});
+		const scaleAppearance = d3.scaleLinear()
+									.domain([0, genres.length])
+									.range([0.1, 1]);
 
-		console.log(source);
-		const names = content
-						.selectAll(".name")
-						.data(source, (d) => `${d.Name}_${d.Sex}`)
-							.enter()
-						.filter((d) => d.Rating.All < 10)
-						.append("g")
-							.attr("class", (d) => `name ${d.Sex == "F" ? "female" : "male"}`)
-							.attr("transform", (d) => 
-								`translate(${scaleGenres(d.Genre) - itemWidth / 2}, ${scaleRating(d.Rating.All)})`);
+		// onclick
+		const btns = d3.select("#top_names_mode_switch")
+						.selectAll("button")
+						.on("click", () => {
+							const curr = d3.select(d3.event.target);
+							btns.attr("class", "");	
+							curr.attr("class", "active");
 
-		names
-			.append("rect")
-			.attr("width", itemWidth)
-			.attr("height", itemHeight);
+							const mode = curr.attr("value");
+							renderNames(mode);						
+						});
 
-		names
-			.append("text")
-			.text((d) => d.Name)
-			.attr("dx", itemWidth / 2)
-			.attr("dy", itemHeight / 2 + 5);
+		btns.node().click();
+
+		function renderNames(mode /*All, F, M*/) {
+			const predicate = (mode == "All") ?
+								(d) => (d.Rating.All < 10) :
+								(d) => (d.Sex == mode);
+
+			const type = (mode == "All") ? "All" : "Gender";
+
+			const filteredData = source.filter(predicate);
+
+			const names = content
+						.selectAll(".item")
+						.data(filteredData, (d) => `${d.Name}_${d.Sex}_${d.Genre}`);
+
+			//remove old
+			names.exit()
+				.transition()
+					.duration(200)
+      				.style("fill-opacity", 1e-6)
+      				.remove();
+
+      		//update old elements
+      		names
+      			.transition()
+      				.duration(600)
+      			.attr("transform", (d) => `translate(${scaleGenres(d.Genre) - itemWidth / 2}, ${scaleRating(d.Rating[type])})`);
+
+      		names
+      			.select("rect")
+      			.transition()
+      				.duration(600)
+				.style("opacity", (d) => scaleAppearance(d.Appearance[type]));
 
 
+      		// enter new elements
+      		const newLabels = names.enter()
+					.append("g")
+						.attr("class", (d) => `item ${d.Sex == "F" ? "female" : "male"}`)
+						.attr("transform", (d) => `translate(${scaleGenres(d.Genre) - itemWidth / 2}, ${height})`);
+
+			newLabels
+				.transition()
+					.delay(200)
+					.duration(600)
+				.attr("transform", (d) => 
+						`translate(${scaleGenres(d.Genre) - itemWidth / 2}, ${scaleRating(d.Rating[type])})`);
+
+			newLabels
+				.append("rect")
+					.attr("width", itemWidth)
+					.attr("height", itemHeight)
+					.style("opacity", (d) => scaleAppearance(d.Appearance[type]));
+
+			newLabels
+				.append("text")
+				.text((d) => d.Name)
+					.attr("dx", itemWidth / 2)
+					.attr("dy", itemHeight / 2 + 5)
+					.attr("stroke", "none")
+					.attr("fill", "black");
+		}
+		
 	});
 
 
