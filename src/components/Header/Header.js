bc697b0f2c226c05c888f7ac6bac8a87a162b985
maryzam <zamo.code@gmail.com>
2018-12-07 00:35:36 +0300

    Add some header styles

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,5 +1,11 @@
 import React, { Fragment } from 'react';
 
-const Header = () => (<header>I'm header</header>);
+const Header = () => (
+	<header>
+		<h1>Cinematic Names</h1>
+		<h4>The story of how I processed million of movies& TV series to ...</h4>
+		<p></p>
+	</header>
+);
 
 export default Header;
