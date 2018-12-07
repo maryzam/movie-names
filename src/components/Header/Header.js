1de5b201a93dc1864c826b169ecbf797e969ec6e
maryzam <zamo.code@gmail.com>
2018-12-07 22:20:52 +0300

    Update story header

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -3,8 +3,10 @@ import React, { Fragment } from 'react';
 const Header = () => (
 	<header>
 		<h1>Cinematic Names</h1>
-		<h4>The story of how I processed million of movies& TV series to ...</h4>
-		<p></p>
+		<p>by <a href="https://twitter.com/MaryZamCode">Mary Zam</a></p>
+		<p className="description">
+			How I processed character names from 3+ million films & TV series and find out the difference between naming in real life and cinema world.
+		</p>
 	</header>
 );
 
