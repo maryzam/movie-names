e1cda647f4738603e12c87e85e8a656e4f6f5012
maryzam <zamo.code@gmail.com>
2019-01-07 22:58:05 +0300

    Fill data&methodology section

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -4,9 +4,28 @@ const Footer = () => (
 	<footer>
 		<div className="content">
 			<h4>Data and Methodology</h4>
-			<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
-			<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
-			<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+			<p>
+				This research is moustly based on two data sets. First one - <a href="https://www.ssa.gov/oact/babynames/index.html" target="_blank" rel="noopener noreferrer">"Baby Names from Social Security Card Applications"</a>, published by Social Security Administration of the USA, contains the information about name, year of birth, sex and total number from a 100 percent sample of Social Security card applications after 1879. Second one  - <a href="" target="_blank" rel="noopener noreferrer">IMDb Datasets</a>,owned and distributed by IMDb.com, provides the various data related to films, television programs and TV series.
+			</p>
+			<p>
+				The period from 1960 to nowadays was chosen as a basic time interval as as long enough to reveal common patterns and key differences between our dayly life and cinematic world and relevant to modern life at the same time. All data, related to early years was ignored for both datasets.
+			</p>
+			<p>
+				"Baby Names from Social Security Card Applications" dataset originally presented all information about naming in the US in already aggregated by years and gender and it didn't reqired any advansed data preprocessing. 
+			</p>
+			<p>
+				On the other hand, I was fully responsible for gathering simmilar data for movies & TS series using overall information about films & additional data about their main and secondary characters. During this process I've excluded not only too old pictures, but also all documantary movies and TV shows, which have their own naming rules bases moustly not on the screenwriter but on an actual names of the heroes and could distort the final results of the research. The final movie list contains 389.187 films, short movies and TV series.
+			</p>
+			<p>
+				All togeter these pictures allowed me to gathering information about names, used by almoust a million cinema characters. I've used a set of text mining algorithms to extract actual first names of heroes and distinguish them from last names, nicknames, official titles & ranks. You just can't imaging how many characters were called "Guy" or "That Old Lady" during the last 70 years!
+			</p>
+			<p>
+				To make the frequency statistic for ciname world more accurate from the list of characters were removed duplicates that mentoned in different movies and/or TV series, but actually references to the same character such as Captain James Kirk, Princess Leia or Mr James Bond.
+			</p>
+			<p>
+				Gender was determined based on gender of a person who used to play the character. I assumed that it's not the most accurate way to determ this parameter and, unfortunatly, it forced me to focused on "female" and "male" cetegories only. However, this assumption shouldn't significantly affect the final conclusions as soon as they explored the most common situations.
+			</p>
+			<p>All source code and result datasets, used for this article are availabe on <a href="https://github.com/maryzam/movie-names"  target="_blank" rel="noopener noreferrer">GitHub</a></p>
 		</div>
 	</footer>
 );
--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -5,7 +5,7 @@ const Header = () => (
 		<h1>Cinematic Names</h1>
 		<p>by <a href="https://twitter.com/MaryZamCode">Mary Zam</a></p>
 		<p className="description">
-			How I processed character names from 3+ million films & TV series and find out the difference between naming in real life and cinema world.
+			How I processed character names from about four hundred thousand films & TV series, shot from 1960 to our days, and found out the difference between naming in real life and cinema world.
 		</p>
 	</header>
 );
